5593ff8f7d0be3845461f5e2200bb01c3f7aad19
ThierrryScotto <thierry_192@outlook.com>
2022-03-05 14:58:40 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from predict_page import show_predic_page
+from explore_page import show_explore_page
+
+page = st.sidebar.selectbox('Explore or Predict', ("Predict", "Explore"))
+if page == "Predict":
+    show_predic_page()
+else:
+    show_explore_page()
---
+++ readme.md
@@ -0,0 +1,6 @@
+### Library
+https://streamlit.io/
+### Dataset
+https://insights.stackoverflow.com/survey
+### GridSearchCV
+https://andersonuyekita.github.io/notebooks/blog/2019/03/21/como-usar-o-gridsearchcv/
